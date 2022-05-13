07c9cc6c10c994558d14fd1132623e324bc39d25
Sonny Tan <sonnythemantan@csu.fullerton.edu>
2022-05-12 21:35:57 -0700

    added yelp dataset instead of API

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-import requests
 import os
+import pandas
+import numpy
+import json
 from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-def requestRestaurants():
-    headers = {'Authorization': 'Bearer {}'.format(API_KEY)}
-    payload = {'location': '92683', 'categories': 'mexican'}
-    r = requests.get('https://api.yelp.com/v3/businesses/search', headers = headers, params = payload)
-    print(r.json())
-    return r.json()
+def readBusinesses():
+    f = open('yelp_academic_dataset_business.json', 'r', encoding="utf8")
+    business = [json.loads(line) for line in f]
+    f.close()
+    return business
+
+def readUsers():
+    f = open('yelp_academic_dataset_user.json', 'r', encoding="utf8")
+    user = [json.loads(line) for line in f]
+    f.close()
+    return user
 
 
 def main():
-    options = requestRestaurants()
-    print(options["businesses"][0])
+    business = readBusinesses()
+    user = readUsers()
+    
+    print(business['business_id'])
+
 
 if __name__ == '__main__':
     main()
