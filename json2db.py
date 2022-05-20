1862f0f643d22256f177a4be2f7202dcda6fae48
Sonny Tan <sonnythemantan@csu.fullerton.edu>
2022-05-19 22:43:13 -0700

    Implemented working top 25 query

---
+++ json2db.py
@@ -0,0 +1,26 @@
+import json
+import csv
+
+with open('yelp_academic_dataset_business.json', 'r', encoding='utf-8') as f:
+    data = [json.loads(line) for line in f]
+
+    business = []
+    index = 1
+    for d in data:
+        if d['city'] == "Philadelphia" and (str(d['categories']).find("Restaurant") != -1 and (str(d['categories']).find("Mexican") != -1)):
+            business.append(d)
+    
+    data_file = open('business_data.csv', 'w', encoding='utf-8')
+    csv_writer = csv.writer(data_file)
+    count = 0
+
+    for b in business:
+        if count == 0:
+
+            header = b.keys()
+            csv_writer.writerow(header)
+            count += 1
+
+        csv_writer.writerow(b.values())
+data_file.close()
+
--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import os
 import pandas
 import numpy
 import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
+df = pandas.read_csv("business_data.csv", engine='python', index_col=False)
+df['index'] = range(0, len(df))
 
+class business():
+    def __init__():
+        name = ""
+        dfIndex = None
+        location = (0, 0)
+        
 def readBusinesses():
     f = open('yelp_academic_dataset_business.json', 'r', encoding="utf8")
-    business = [json.loads(line) for line in f]
+    input = [json.loads(line) for line in f]
+    business = []
     f.close()
+    for i in input:
+        if i['city'] == "Philadelphia":
+            business.append(i)
     return business
 
 def readUsers():
@@ -18,12 +32,74 @@ def readUsers():
     f.close()
     return user
 
+def readReviews():
+    f = open('yelp_academic_dataset_review.json', 'r', encoding="utf8")
+    user = [json.loads(line) for line in f]
+    f.close()
+    return user
+
+def index_from_name(name):
+    return df[df.name == name]["index"].values[0]
+
+def name_from_index(index):
+    return df[df.index == index]["name"].values[0]
+
+def dfLookup(index, lookup):
+    return df[df.index == index][lookup].values[0]
+
+def dictLookup(dict, lookup):
+    for key, value in dict.items():
+        if (value == lookup):
+            print(True)
+            return True
+        else:
+            print(False)
+            return False
+
+def sanitizeChains(ranked_similar_restaurants, name):
+    ret = dict([])
+    count = 0
+    for r in ranked_similar_restaurants:
+        restname = name_from_index(r[0])
+        if restname != name and count < 24 and (restname not in ret.values()):
+            ret[r[0]] = restname 
+            count += 1
+    return ret
+    
 
 def main():
-    business = readBusinesses()
-    user = readUsers()
+    factors = ['categories']
+
+    for factor in factors:
+        df[factor] = df[factor].fillna('')
+
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df["categories"])
+    print("Count Matrix:", count_matrix.toarray())
+
+    cosine_sim = cosine_similarity(count_matrix)
+    numpy.savetxt('F:/array.txt', cosine_sim)
+
+    restaurant_user_likes = "Xochitl"
+
+    restaurant_index = index_from_name(restaurant_user_likes)
+
+    similar_restaurants = list(enumerate(cosine_sim[restaurant_index]))
+
+    ranked_similar_restaurants = sorted(similar_restaurants, key=lambda x:x[1], reverse=True)
+
+    count = 0
+
+    top25 = sanitizeChains(ranked_similar_restaurants, restaurant_user_likes)
+
+    print(top25.values())
+
     
-    print(business['business_id'])
+    
+
+    
+    
+
 
 
 if __name__ == '__main__':
